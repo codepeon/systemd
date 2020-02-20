36a82a15603f04bcdf7c2c7800fd5fbfbbc7169a
Yu Watanabe <watanabe.yu+github@gmail.com>
2020-02-21 01:04:50 +0900

    unit: introduce systemd-networkd@.service and friends

--- src/network/networkd-network.h
+++ src/network/networkd-network.h
@@ -82,6 +82,8 @@ struct Network {
 
         /* Master or stacked netdevs */
         NetDev *batadv;
+        char *namespace;
+
         NetDev *bridge;
         NetDev *bond;
         NetDev *vrf;
@@ -347,6 +349,7 @@ CONFIG_PARSER_PROTOTYPE(config_parse_required_for_online);
 CONFIG_PARSER_PROTOTYPE(config_parse_keep_configuration);
 CONFIG_PARSER_PROTOTYPE(config_parse_ipv6_link_local_address_gen_mode);
 CONFIG_PARSER_PROTOTYPE(config_parse_activation_policy);
+CONFIG_PARSER_PROTOTYPE(config_parse_namespace);
 
 const struct ConfigPerfItem* network_network_gperf_lookup(const char *key, GPERF_LEN_TYPE length);
 
